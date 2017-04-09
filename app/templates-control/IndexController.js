dedd14f2810e23578580cf5ec69693985758b143
patrik <prefixdev666@gmail.com>
2017-04-09 22:32:23 +0200

    Core

--- app/templates-control/IndexController.js
+++ app/templates-control/IndexController.js
@@ -2,18 +2,20 @@
 const fs = require("fs");
 const mu = require("mu2");
 
+/* classes */
+const Controller = require("../modules/Controller.js");
+
 /* helpers */
 const settings = require("../modules/settings.js");
 
-
 /* @-<IndexController ********************************************************/
 /*****************************************************************************/
-class IndexController
+class IndexController extends Controller
 {
   /* @@-<constructor *********************************************************/
   /***************************************************************************/
   constructor()
-  {
+  {console.log(super().a);
     this.classId = this.constructor.name.split("Controller")[0].toLowerCase();
     if(this.classId === "index"){ this.classId = ""; }
 
@@ -26,10 +28,10 @@ class IndexController
     let controllerData = {};
     let data = {};    
     const methodId = "default";
-    const templateData = JSON.parse(fs.readFileSync(settings.root + "/app/templates-data/" + this.classId + "/" + methodId + ".json", "utf8"));
+    const jsonData = JSON.parse(fs.readFileSync(settings.root + "/app/templates-data/" + this.classId + "/" + methodId + ".json", "utf8"));
     
     /* connect controller + .json data and compile the template */
-    data = { "a" : "b", templateData };
+    data = { controllerData, jsonData };
     const compiledTemplate = mu.compileAndRender(settings.root + "/app/templates-view/" + this.classId + "/" + methodId + ".mustache", data);
 
     /* event - data - compiledTemplate ***************************************/
